77199f2c22522821fbff021a3d64c7a85fc9c4c6
LuisChamo <chamo_luis@yahoo.es>
2016-11-11 14:10:06 -0600

    Segundo cambio

--- CalculadoraProyecto.java
+++ CalculadoraProyecto.java
@@ -1,35 +1,36 @@
 import java.util.Scanner;
 
-public class ProyectoCalculadora {
+public class CalculadoraProyecto {
 
 public static void main(String[] args )  {
 Scanner entrada=new Scanner(System.in);
 String almacenamiento[]= new String [7];
 double valor, valor2, valor3;
 int dato4, calculadora, borrar;
+int numero;
        
 	do {
-	System.out.println("Bienvenidos a mi Proyecto Calculadora");        
-	System.out.println("1: Suma de dos numeros");
-	System.out.println("2: Resta de dos numeros");
-	System.out.println("3: Multiplicacion de dos numeros");
-	System.out.println("4: Division de dos numeros");
-	System.out.println("5: Ver bitacora");
-	System.out.println("6: Borrar bitacora");
+	System.out.println("\nBienvenidos a mi Proyecto Calculadora\n");        
+	System.out.println("1: Suma de 2 Numeros");
+	System.out.println("2: Resta de 2 Numeros");
+	System.out.println("3: Multiplicacion de 2 Numeros");
+	System.out.println("4: Division de 2 Numeros");
+	System.out.println("5: Ver Bitacora");
+	System.out.println("6: Borrar Bitacora");
 	System.out.println("7: Salir");
-	System.out.println("\nSeleccione la opcion deseada");
+	System.out.println("\nSeleccione una opcion:");
 	calculadora=entrada.nextInt();
 
 	switch(calculadora){
 
 	case 1:
-	   	System.out.println("\nUsted selecciono la opcion 1. Suma de dos numeros ");
-		System.out.println("Ingrese el primer numero ");
+	   	System.out.println("\nUsted selecciono la opcion 1. Suma de 2 numeros ");
+		System.out.println("\nIngrese el primer numero: ");
 		valor=entrada.nextDouble();
-		System.out.println("Ingrese el segundo numero ");
+		System.out.println("\nIngrese el segundo numero: ");
 		valor2=entrada.nextDouble();
 		valor3=valor+valor2;
-		System.out.println("El resultado es " + valor3);
+		System.out.println("El resultado es: " + valor3);
 		System.out.println("\n");
 		if(almacenamiento[0]==null){
 		almacenamiento[0]=(valor + " + " + valor2 + " = " + valor3);
@@ -64,14 +65,19 @@ int dato4, calculadora, borrar;
 		}
 		}
 		}
+		{
+			System.out.println("Ingrese cualquier numero para volver a Menu:");
+			numero=entrada.nextInt();
+		}
+
 	break;
 
 
 	case 2:
-		System.out.println("Usted selecciono la opcion 2: Resta de dos numeros ");
-		System.out.println("Ingrese el primer numero: ");
+		System.out.println("\nUsted selecciono la opcion 2: Resta de 2 Numeros ");
+		System.out.println("\nIngrese el primer numero: ");
 		valor=entrada.nextDouble();
-		System.out.println("Ingrese el segundo numero: ");
+		System.out.println("\nIngrese el segundo numero: ");
 		valor2=entrada.nextDouble();
 		valor3=valor-valor2;
 		System.out.println("El resultado es: " +  valor3);
@@ -109,14 +115,18 @@ int dato4, calculadora, borrar;
 		}
 		}
 		}
+		{
+			System.out.println("Ingrese cualquier numero para volver a Menu");
+			numero=entrada.nextInt();
+		}
 	break;
 
 
 	case 3:
-		System.out.println("Usted selecciono la opcion 3: Mutiplicacion de dos numeros ");
-		System.out.println("Ingrese el primer numero: ");
+		System.out.println("\nUsted selecciono la opcion 3: Mutiplicacion de 2 numeros. ");
+		System.out.println("\nIngrese el primer numero: ");
 		valor=entrada.nextDouble();
-		System.out.println("Ingrese el segundo numero: ");
+		System.out.println("\nIngrese el segundo numero: ");
 		valor2=entrada.nextDouble();
 		valor3=valor*valor2;
 		System.out.println("El resultado es: " + valor3);
@@ -154,17 +164,21 @@ int dato4, calculadora, borrar;
 		}
 		}
 		}
+		{
+			System.out.println("Ingrese cualquier numero para volvea Menu:");
+			numero=entrada.nextInt();
+		}
 	break;
 
 
 	case 4:
-		System.out.println("Usted selecciono la opcion 4: Division de dos numeros ");
-		System.out.println("Ingrese el primer numero: ");
+		System.out.println("\nUsted selecciono la opcion 4: Division de 2 numeros. ");
+		System.out.println("\nIngrese el primer numero: ");
 		valor=entrada.nextDouble();
-		System.out.println("Ingrese el segundo numero: ");
+		System.out.println("\nIngrese el segundo numero: ");
 		valor2=entrada.nextDouble();
 		if (valor2==0){
-		System.out.println("Error: La operacion de Dividir entre 0 ¡NO ESTA PERMITIDA!");
+		System.out.println("Error: La operacion Dividir entre 0 *NO ESTA PERMITIDA* ");
 		System.out.println("\n");
 		if(almacenamiento[0]==null){
 		almacenamiento[0]=(valor + " / " + valor2 + " = " + "Error");
@@ -200,7 +214,8 @@ int dato4, calculadora, borrar;
 		}
 		}
 		}
-		else {
+		
+		else{
 		valor3=valor/valor2;
 		System.out.println("El resultado es: " +  valor3);
 		System.out.println("\n");    
@@ -238,11 +253,15 @@ int dato4, calculadora, borrar;
 		}
 		}
 		}
+		{
+			System.out.println("Ingrese cualquier numero para volver a Menu:");
+			numero=entrada.nextInt();
+		}
 	break;
 		
 		
 	case 5:
-		System.out.println("Usted selecciono la opcion 5: Ver bitacora ");
+		System.out.println("\nUsted selecciono la opcion 5: Ver bitacora. ");
 		for (dato4=0;dato4<=6;dato4++){
 		if(almacenamiento[dato4]==null){
 		System.out.println("");
@@ -252,34 +271,41 @@ int dato4, calculadora, borrar;
 		}
 		} 
 		System.out.println("\n");
+		{
+			System.out.println("Ingrese cualquier numero para volver a Menu:");
+			numero=entrada.nextInt();
+		}
 		break;
 		
 	case 6:
-	System.out.println("\nUsted a elegido la opcion 6. Borrar Bitacora ");
-	System.out.println("\nEsta seguro de borrar la Bitacora");
-	System.out.println("1: Si");
-	System.out.println("2: No");
-	System.out.println("\nIngrese un numero:");
-	borrar=entrada.nextInt();
-	if (borrar==1){
-	for (dato4=0;dato4<=5;dato4++){
-	almacenamiento[dato4]=null;
-	}
-	System.out.println("\nEspere... Su Bitacora esta siendo borrada.");
-	System.out.println("\nBitacora Borrada\n ");
-	}
-	else{
-	System.out.println("\nSu bitacora no fue Borrada.\n");
+		System.out.println("\nUsted a elegido la opcion 6. Borrar Bitacora ");
+		System.out.println("\n¿Esta seguro de borrar la Bitacora? ");
+		System.out.println("1: Si");
+		System.out.println("2: No");
+		System.out.println("\nIngrese un numero:");
+			borrar=entrada.nextInt();
+		if (borrar==1){
+		for (dato4=0;dato4<=5;dato4++){
+		almacenamiento[dato4]=null;
+		}
+		System.out.println("\nEspere... Su Bitacora esta siendo borrada.");
+		System.out.println("\nBitacora Borrada\n ");
+		}
+	
+
+		else{
+		System.out.println("\nSu bitacora no fue Borrada.\n");
+		}
+		{
+		System.out.println("Ingrese cualquier numero para volver a Menu:");
+		numero=entrada.nextInt();
 	}
 	break;
 	default:
 	if(calculadora!=7){
-	System.out.println("Numero fuera de rango establecido");
+	System.out.println("Numero ingresado esta fuera del rango establecido.");
 	}
 	}
-	}
-
-	while(calculadora!=7);
-
+	}while(calculadora!=7);
 }
 }
